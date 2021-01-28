b34a77174cce70ac3babd234ab87c64954ae1038
adrianowy <adrianoabreu.wy@gmail.com>
2021-01-28 14:17:01 -0400

    Initial Commit

---
+++ src/components/layout/TopBar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+      paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: 'none',
+    }
+}));
+
+export default function TopBar(props){
+
+    const classes = useStyles();
+
+    return (
+      <AppBar position="absolute" className={clsx(classes.appBar, props.sideBarStatus && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={props.handleDrawerOpen}
+            className={clsx(classes.menuButton, props.sideBarStatus && classes.menuButtonHidden)}
+          >
+          
+            <Icon>menu</Icon>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    )
+}
